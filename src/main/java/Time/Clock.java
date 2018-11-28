4aa6eafd8c118400c214fd1741f76c0c1c5cfd8d
Gurvir <j0q1b@ugrad.cs.ubc.ca>
2018-11-27 16:17:49 -0800

    Added a background for the gui and a icon that changes with the weather. Also added a button that allows you to add to your list of cities

--- src/main/java/Time/Clock.java
+++ src/main/java/Time/Clock.java
@@ -17,38 +17,28 @@ public class Clock extends Observable implements ActionListener {
         String time = "";
 
         GregorianCalendar calendar = new GregorianCalendar();
-        int h = calendar.get(GregorianCalendar.HOUR_OF_DAY);
-        int m = calendar.get(GregorianCalendar.MINUTE);
-        int s = calendar.get(GregorianCalendar.SECOND);
-        time += ((h < 10) ? "0" : "") + h + ":";
-        time += ((m < 10) ? "0" : "") + m + ":";
-        time += ((s < 10) ? "0" : "") + s;
+        int hour = calendar.get(GregorianCalendar.HOUR_OF_DAY);
+        int minutes = calendar.get(GregorianCalendar.MINUTE);
+        int seconds = calendar.get(GregorianCalendar.SECOND);
+        // This block of code is to ensure that the time is formatted correctly.
+        time += ((hour < 10) ? "0" : "") + hour + ":";
+        time += ((minutes < 10) ? "0" : "") + minutes + ":";
+        time += ((seconds < 10) ? "0" : "") + seconds;
 
         return time;
     }
 
+    @Override
     public void actionPerformed(ActionEvent e) {
         setChanged();
         notifyObservers(this.getTime());
     }
 
     public void startClock() {
-        if (timer == null) {
+        if (timer == null) { // if timer is null then, it needs to be started
             timer = new javax.swing.Timer(1000, this);
             timer.setInitialDelay(0);
             timer.start();
         }
     }
-
-//    public void restartClock(){
-//        if (!timer.isRunning()){
-//            timer.restart();
-//        }
-//    }
-//
-//    public void stopClock() {
-//        if (timer != null) {
-//            timer.stop();
-//        }
-//    }
 }
